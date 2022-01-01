3c854be24064d543a3d2c293fb9af12bb851caef
Le Minh Nghia <nghia9c.12@gmail.com>
2022-01-02 00:11:04 +0700

    fix backend returnbike

--- Software_Programing/back-end/src/main/java/com/sapo/repositories/InvoiceRepository.java
+++ Software_Programing/back-end/src/main/java/com/sapo/repositories/InvoiceRepository.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer>, JpaSpecificationExecutor<Invoice> {
-//    @Query(value = "select invoices.* from invoices where invoices.vehicle_id = ?", nativeQuery = true)
-//    Invoice findInvoiceByVehicleIdAndStatus(int vehicleId, int status);
+    @Query(value = "select invoices.* from invoices where invoices.vehicle_id = ?", nativeQuery = true)
+    Invoice findInvoiceByVehicleIdAndStatus(int vehicleId, int status);
     Invoice findById(int invoiceId);
 
-    Invoice findInvoiceByVehicleIdAndStatus(int vehicleId, int status);
+//    Invoice findInvoiceByVehicleIdAndStatus(int vehicleId, int status);
 }
