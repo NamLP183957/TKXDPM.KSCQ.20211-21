858e57e544390021df2257916bf233cf64356cd2
datbt3 <killer10011803@gmail.com>
2022-01-05 11:42:46 +0700

    update javadoc uc vehicle information

--- Software_Programing/back-end/src/main/java/com/sapo/controllers/VehicleController.java
+++ Software_Programing/back-end/src/main/java/com/sapo/controllers/VehicleController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+/**
+ * Class nay chua cac phuong thuc lấy thông tin xe trong bãi và xe đang thuê
+ * @author Dat_BT_20183880
+ */
 @RequestMapping("/api/vehicles")
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -19,25 +22,43 @@ public class VehicleController {
         this.vehicleService = vehicleService;
     }
 
+    /**
+     * Lấy list xe trong bãi xe được chọn
+     * @variable stationId: id của bãi xe
+     * @return vehicles: thong tin xe
+     */
   @GetMapping("/list/{stationId}")
   public ResponseEntity<List<Vehicle>> getListVehicleInStation(@PathVariable("stationId") int stationId){
       List<Vehicle> vehicles = vehicleService.getListVehicleInStation(stationId);
       return ResponseEntity.ok(vehicles);
   }
 
-    //API tìm thông tin Vehicle bằng id
+    /**
+     * Lấy thông tin xe theo id xe
+     * @variable id: id của xe
+     * @return vehicles: thong tin xe
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Vehicle> findVehicleById(@PathVariable("id") int id){
         Vehicle vehicle = vehicleService.findVehicleById(id);
         return ResponseEntity.ok(vehicle);
     }
 
+    /**
+     * Lấy thông tin xe theo id xe
+     * @return vehicles: thong tin xe đang thuê
+     */
     @GetMapping("/list-in-rent-time")
     public ResponseEntity<List<VehicleDTOResponse>> getListVehicleInRentTime(){
         List<VehicleDTOResponse> vehicles = vehicleService.getListVehicleInRentTime();
         return ResponseEntity.ok(vehicles);
     }
 
+    /**
+     * Lấy thông tin xe theo id xe
+     * @variable id: id của xe đang thuê
+     * @return vehicles: thong tin xe đang thuê
+     */
     @GetMapping("/list-in-rent-time/{id}")
     public ResponseEntity<VehicleDTOResponse> findVehicleInRentTimeById(@PathVariable("id") int id){
         VehicleDTOResponse vehicleDTOResponses = vehicleService.findVehicleInRentTimeById(id);
--- Software_Programing/font_end/src/services/VehicleService.js
+++ Software_Programing/font_end/src/services/VehicleService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const VEHICLE_API_URL = "http://localhost:8080/api/vehicles";
+import {SERVER_PORT} from '../variables/Server'
+const VEHICLE_API_URL = `http://localhost:${SERVER_PORT}/api/vehicles/`
 
 const getListVehicleInStation = (stationId) => {
     return axios.get(VEHICLE_API_URL + "/list/" + stationId);
